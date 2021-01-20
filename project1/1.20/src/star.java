267426aabdc2516592bb757c8922997513fa07e7
ntdgy <ntdgy2001@gmail.com>
2021-01-20 11:34:50 +0800

    lab2

---
+++ project1/1.20/src/star.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class star {
+    public static void main(String[] args) {
+        System.out.println("*********    ***      *        *");
+        System.out.println("*       *   *   *    ***      *  *");
+        System.out.println("*       *  *     *  *****    *    *");
+        System.out.println("*       *  *     *    *     *      *");
+        System.out.println("*       *  *     *    *    *        *");
+        System.out.println("*       *  *     *    *     *      *");
+        System.out.println("*       *  *     *    *      *    *");
+        System.out.println("*       *   *   *     *       *  *");
+        System.out.println("*********    ***      *        *");
+    }
+
+}
--- project1/1.20/src/timecal.java
+++ project1/1.20/src/timecal.java
@@ -2,8 +2,15 @@ import java.util.*;
 public class timecal {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("please input the number og seconds");
+        System.out.println("please input the number of seconds");
         int t = s.nextInt();
+        int h,min,sec,c;
+        sec = t%60;
+        c = t/60;
+        min = c%60;
+        h = c/60;
+        System.out.printf("the equivalent time is %d hours %d minutes %d seconds",h,min,sec ) ;
+
 
 
     }
