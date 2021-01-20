5c961c802d8cb7efb0f55a239cda451cc15334ed
ntdgy <ntdgy2001@gmail.com>
2021-01-20 10:59:04 +0800

    lab2

--- project1/src/name.java
+++ project1/src/name.java
@@ -1,5 +1,6 @@
 import java.util.*;
-public class name {
+//util.*;
+public class name{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         System.out.println ("please input your name:");
