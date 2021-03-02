80bffc6daa546fce982e40a29cd80cdb9bc3e995
ntdgy <ntdgy2001@gmail.com>
2021-03-02 10:43:57 +0800

    3.2

--- project1/assignment1/src/number2.java
+++ project1/assignment1/src/number2.java
@@ -6,7 +6,7 @@ public class number2 {
         int b = s.nextInt();
         char m='c';
         while(m!='E'){
-            m = s.next().charAt(0);
+            m = s.next().charAt(0);//注意要加0，表示第一个字符。
             if(m == 'a') a -=1;
             else if (m == 'w') b+=1;
             else if (m == 's') b-=1;
