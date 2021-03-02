08dfd69dfa778728e9666cef54d84a97c6962943
ntdgy <ntdgy2001@gmail.com>
2021-03-02 10:26:18 +0800

    3.2

---
+++ project1/assignment1/src/number1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class number1 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String name = s.next();
+        int id = s.nextInt();
+        if (id>=1500000&&id<12100000) {
+            System.out.println(name + ",welcome to Baoneng City!");
+            System.out.println(name + " " + id);
+        }
+    }
+}
