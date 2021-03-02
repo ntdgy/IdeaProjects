48434a1a942c1f1426f726d35fb8a11c39ae4cd3
ntdgy <ntdgy2001@gmail.com>
2021-03-02 10:52:03 +0800

    3.2

--- project1/assignment1/src/number3.java
+++ project1/assignment1/src/number3.java
@@ -1,6 +1,19 @@
 import java.util.*;
     public class number3 {
         public static void main(String[] args){
+            Scanner s = new Scanner(System.in);
+            int a1 = s.nextInt();
+            int b1 = s.nextInt();
+            int c1 = s.nextInt();
+            int a2 = s.nextInt();
+            int b2 = s.nextInt();
+            int c2 = s.nextInt();
+            int a3 = a2 - a1;
+            int b3 = b2 - b1;
+            int c3 = c2 - c1;
+            if (a3!=0) System.out.print(a3 +"h");
+            if (b3!=0) System.out.print(b3 +"m");
+            if (a3==0&b3==0&&c3==0) System.out.print("0s");
 
         }
 }
