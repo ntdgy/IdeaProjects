57e985b412e165e1e1d8ab47b970cf03c568ad54
ntdgy <ntdgy2001@gmail.com>
2021-03-03 12:02:16 +0800

    3.3

--- project1/assignment1/src/lab3_e3.java
+++ project1/assignment1/src/lab3_e3.java
@@ -2,15 +2,15 @@ import java.util.*;
 public class lab3_e3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Please input your score");
-        int score = s.nextint();
+        System.out.print("Please input your score:");
+        int score = s.nextInt();
         float a;
         a = score;
         float gpa;
         if(score>=60) System.out.println("you have passed the exam");
-          else System.out.println("\n" +
-                "            case 10 : gpa=4.0f;break;you have failed the exam");
-        switch (a/10){
+          else System.out.println("you have failed the exam");
+        switch (score/10){
+            case 10 : gpa=4.0f;break;
             case 9 : gpa=4.0f;break;
             case 8 : gpa=3.0f;break;
             case 7 : gpa=2.0f;break;
