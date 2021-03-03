598d03ba6d3d5dd593934c884fbb1d657e025fac
ntdgy <ntdgy2001@gmail.com>
2021-03-03 11:59:39 +0800

    3.3

--- project1/assignment1/src/lab3_e3.java
+++ project1/assignment1/src/lab3_e3.java
@@ -2,14 +2,22 @@ import java.util.*;
 public class lab3_e3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Silver");
-        int score = s.nextInt();
+        System.out.print("Please input your score");
+        int score = s.nextint();
+        float a;
+        a = score;
         float gpa;
         if(score>=60) System.out.println("you have passed the exam");
-          else System.out.println("you have failed the exam");
-        switch (score/10){
-            case 10 :
+          else System.out.println("\n" +
+                "            case 10 : gpa=4.0f;break;you have failed the exam");
+        switch (a/10){
+            case 9 : gpa=4.0f;break;
+            case 8 : gpa=3.0f;break;
+            case 7 : gpa=2.0f;break;
+            case 6 : gpa=1.0f;break;
+            default: gpa=0.0f;break;
         }
+        System.out.printf("Your score is %.1f, your gpa is %.1f",a,gpa);
 
 
     }
