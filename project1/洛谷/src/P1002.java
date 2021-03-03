0630d0d0423ae047ce5863f7375cc5a11e04cb27
ntdgy <ntdgy2001@gmail.com>
2021-03-03 10:50:36 +0800

    3.3

---
+++ project1/洛谷/src/P1002.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class P1002 {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+        int a,b,c,d;
+        a = s.nextint();
+        b = s.nextint();
+        c = s.nextint();
+        d = s.nextint();
+        int[][] n = new int [22][22];
+
+    }
+
+}
