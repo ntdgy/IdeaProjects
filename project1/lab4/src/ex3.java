a86abbce149d3221cc1618903598a23d68c66106
ntdgy <ntdgy2001@gmail.com>
2021-03-10 11:13:43 +0800

    3.10

--- project1/lab4/src/ex3.java
+++ project1/lab4/src/ex3.java
@@ -1,2 +1,17 @@
+import java.util.*;
 public class ex3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("please input n");
+        int n = s.nextInt();
+        double k=0;
+        for(int i=1;i<=n;i++)
+        {
+            double b = 4 /(float) (2*i-1);
+            if(i%2!=0) k +=b;
+             else k -=b;
+        }
+        System.out.printf("%f",k);
+    }
 }
