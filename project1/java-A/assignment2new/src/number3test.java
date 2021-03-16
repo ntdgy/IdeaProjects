60b81f2a1d23afb6f636a9709eae455f15e75f54
ntdgy <ntdgy2001@gmail.com>
2021-03-16 22:44:02 +0800

    3.16

--- project1/java-A/assignment2new/src/number3test.java
+++ project1/java-A/assignment2new/src/number3test.java
@@ -7,6 +7,6 @@ public class number3test {
 //        BigInteger b = new BigInteger(s.next(),2);
 //        BigInteger r = a.add(b);
 //        System.out.println(r.toString(2));
-          System.out.print(s.nextBigInteger().add(s.nextBigInteger()));//两行OJ不是梦
+          System.out.print(s.nextBigInteger(2).add(s.nextBigInteger(2)));//两行OJ不是梦
     }
 }
